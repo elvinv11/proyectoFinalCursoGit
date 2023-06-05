fdbca9866491e2c40bbe5a994c55ff9ddec56fc7
elvin vargas <elvinv11@gmail.com>
2023-06-04 23:36:41 -0400

    cree el fichero ejersicio1.py y añadi el codigo

---
+++ ejersicio1.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+
+
+a = 0 
+while a >= 0:
+ a = int(input("Ingrese los Kilometros recorridos: "))
+ if a >= 0:
+  b = int(input("cuantos litros de gazolina usò: "))
+  print("Estimado conductor usted a usado, 1 litros de combustible cada", a / b, "kilometro recorrido")
+
+print("Gracias por usar este programa. Adios")
+
+
+
